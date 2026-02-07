0ed8652482709a1186a59d2266b255d00197be29
mayadanesi21-cpu <mayadanesi21@gmail.com>
2026-02-07 23:20:36 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// Allow importing SVGs as React components
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
